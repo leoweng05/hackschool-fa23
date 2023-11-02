d7ccc96e8bf44f7b8587368b61df0e3ad422c833
Leo Weng <leoweng05@gmail.com>
2023-11-01 17:47:18 -0700

    merge

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,36 +1,10 @@
 import Image from 'next/image';
-<<<<<<< HEAD
-import styles from '../styles/Home.module.css'
-import acmlogopng from '../assets/acmlogo.png'
-=======
 import styles from '../styles/Home.module.css';
 import acmLogo from '../assets/acmlogo.png';
->>>>>>> upstream/part4
 
 
 function Home() {
   return (
-<<<<<<< HEAD
-    
-    /* Each function in JSX can only return ONE DOM element, so we have one top-level div */
-    <div className={styles.main}>
-        <div className={styles.welcome}>
-          <h1> Welcome! </h1>
-          <h2>Are you ready to play?</h2>
-          <p>This was made by a student who has no idea what they are doing so be prepared for any inevitable bugs. I really did
-            try my best but it is what it is.
-          </p>
-        </div>
-      <div className={styles.image}>
-        {/* Put the ACM logo here! */}
-        <Image
-        src = {acmlogopng}
-        width ={500}
-        height={500}
-        alt="acm logo"
-        />
-        {/* Keep in mind that any images you want to use must be imported from the /assets folder */}
-=======
     <div className={styles.main}>
         <div className={styles.welcome}>
           <h2>Welcome to HackRacer!</h2>
@@ -39,7 +13,6 @@ function Home() {
           </p>
       </div>
       <div className={styles.image}><Image src={acmLogo} width={200} height={200} alt="acmlogo"/>
->>>>>>> upstream/part4
       </div>
     </div>
   );
